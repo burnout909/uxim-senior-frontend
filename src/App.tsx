07582471c1343f3143bf079353952e3bdce5ecef
burnout909 <newhorizon2232@gmail.com>
2025-04-29 20:31:21 +0900

    add krds design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { Accordion, Breadcrumb, Pagination } from "@krds-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="flex border-[1px] w-full h-full">hi</div>
+      <div className="flex flex-col w-full h-full">
+        <Breadcrumb
+          items={[{ label: <span className="text-[20px]">홈</span> }]}
+        ></Breadcrumb>
+        <Pagination
+          currentPage={9}
+          onPageChange={function ra() {}}
+          totalPages={20}
+          visiblePages={5}
+        />
+        <Accordion
+          items={[
+            {
+              children: (
+                <p className="text-[100px]">Accordion Item 1 Content</p>
+              ),
+              title: "Accordion Item 1",
+            },
+            {
+              children: "Accordion Item 2 Content",
+              title: "Accordion Item 2",
+            },
+            {
+              children: "Accordion Item 3 Content",
+              title: "Accordion Item 3",
+            },
+          ]}
+        />
+      </div>
     </>
   );
 }
