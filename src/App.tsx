ca5e3ee6c93280487162489c7b6d6106b7c790da
hasyungsyung <kg191719@naver.com>
2025-05-21 10:13:51 +0900

    breadcrumbs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
 import { Accordion, Breadcrumb, Pagination } from "@krds-ui/core";
 import "./App.css";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Intro from "./pages/Intro.tsx";
+import Club from "./pages/Club.tsx";
+import { FiHome } from "react-icons/fi";
 
 function App() {
+  let navigate = useNavigate();
+
+
   return (
     <>
       <div className="flex flex-col w-full h-full">
         <Breadcrumb
           items={[{ label: <span className="text-[20px]">홈</span> }]}
         ></Breadcrumb>
-        <div className='main-container'>
-          <h1 className="title">시니어클럽소개</h1>
 
-          <div className="content-container">
-            <h1 style={{fontSize: '27px', marginBottom: '20px'}}> • 시니어클럽이란?</h1>
-            <p style={{fontSize: '24px'}}>
-              시니어클럽은 대한민국에서 만 60세 이상의 어르신들에게 일자리와 사회참여 기회를 제공하기 위해 운영되는 기관입니다. 
-              보건복지부의 지원 아래 각 지방자치단체와 민간 복지기관 등이 함께 참여하여 지역별로 운영되며, 
-              고령화 사회 속에서 노인의 건강한 노후생활과 경제적 자립을 지원하는 중요한 역할을 하고 있습니다.
-            </p>
-          </div>
+        <>
+          <div className="flex flex-col w-full h-full bg-gray-200" >
+            <div>
+              <Breadcrumb 
+                items={[
+                  {
+                    label: <span className="text-lg"><FiHome /></span>,
+                    onClick: () => {
+                      navigate("/home");
+                    },
+                  },
+                  {
+                    label: <span className="text-lg">기관소개</span>,
+                    onClick: () => {
+                      navigate("/intro");
+                    },
+                  },
+                  {
+                    label: <span className="text-lg">시니어클럽소개</span>,
+                    onClick: () => {
+                      navigate("/club");
+                    },
+                  },
+                ]}
+              ></Breadcrumb>
 
-          <div>
-            <h1 style={{fontSize: '27px', marginBottom: '20px'}}> • 노인일자리 및 사회활동지원사업 유형</h1>
-            <div className="img-container">
-              <img width='211px' height='265px'></img>
-              <img width='211px' height='265px'></img>
-              <img width='211px' height='265px'></img>
-              <img width='211px' height='265px'></img>
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/intro" element={<Intro />} />
+                <Route path="/club" element={<Club />} />
+              </Routes>
             </div>
           </div>
-        </div>
+        </>
+        
 
         <Pagination
           currentPage={9}
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,6 @@
+export default function Intro() {
+    return (
+    <h1>{"기관소개 페이지입니다"}</h1>
+    )
+  }
+  
