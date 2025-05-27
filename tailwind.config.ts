e274ff1f9979d970360b5e2a09b215fad6f07253
konhoe <wjstk7973@gmail.com>
2025-05-27 14:59:14 +0900

    Update plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import krdsPlugin from "@krds-ui/tailwindcss-plugin";
+import typography from "@tailwindcss/typography"; // ✅ 추가
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {},
-  plugins: [krdsPlugin],
+  plugins: [krdsPlugin, typography], // 추가
+  
 };
